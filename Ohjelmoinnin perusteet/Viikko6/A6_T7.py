304bc598e7f9491521be30eb95edbef42bbf9d90
Andrei Kuga <andrei.kuga@student.lab.fi>
2025-10-14 02:42:46 +0300

    Viikko 6 T7

--- Ohjelmoinnin perusteet/Viikko6/A6_T7.py
+++ Ohjelmoinnin perusteet/Viikko6/A6_T7.py
@@ -17,19 +17,19 @@
 
 # Create a file “player_progress.txt” and initialize it with the following details.
 
-# file 1
+# "player_progress.txt" start
 # current_location;next_location;passphrase
 # 0;1;qvfpvcyvar
 
 # Player progress file explained:
 
-# First row is the header row with the column names.
-# Data row 1
-#   Current location id 0 refers to the starting point (Home).
-#   Next location id 1 refers to the next objective (Galba's palace).
-#   Passphrase ciphered (ROT13)
-# Next data row
-#   Should be added after progress is made on it’s own new line in the same file.
+# 1 First row is the header row with the column names.
+# 2 Data row 1
+#       1 Current location id 0 refers to the starting point (Home).
+#       2 Next location id 1 refers to the next objective (Galba's palace).
+#       3 Passphrase ciphered (ROT13)
+# 3 Next data row
+#       1 Should be added after progress is made on it’s own new line in the same file.
 # Once you have traveled to the destination, walk into the palace and shout the passphrase(print the plain version) 
 # to the guard as you enter. After entering, locate the message (open file "{NextLocationId}_{PassPhrase}.gkg") and 
 # read the content.
@@ -44,11 +44,50 @@
 
 # file1: Ciphered message "{NextLocationId}_{PassPhrase}.gkg"
 # file2: Plain version to save "{NextLocationId}-{PlainPassPhrase}.txt"
+
+# file 1:
+# 0;1;qvfpvcyvar
+# Cneg 0 - Lrne bs gur Sbhe Rzcrebef:
+
+# Va NQ 68, nsgre Areb'f qrngu, Ebzr cyhatrq vagb punbf.
+# Jvgu ab pyrne urve, gur rzcver fnj encvq cbjre fgehttyrf.
+# Tnyon gbbx gur guebar svefg, sbyybjrq ol Bgub, Ivgryyvhf, naq svanyyl Irfcnfvna,
+# rnpu onggyvat sbe pbageby va jung orpnzr gur Lrne bs gur Sbhe Rzcrebef.
+
+# file 2:
+# Part 0 - Year of the Four Emperors:
+
+# In AD 68, after Nero's death, Rome plunged into chaos.
+# With no clear heir, the empire saw rapid power struggles.
+# Galba took the throne first, followed by Otho, Vitellius, and finally Vespasian,
+# each battling for control in what became the Year of the Four Emperors.
+
+# After the progress and the Emperor’s message have been saved, the program closes with the final phrases. 
+# The next time the program runs, it should be able to read the previous progress from player_progress.txt 
+# and continue the next turn.
+
+# Example program run:
+
+# Travel starting.
+# Currently at home.
+# Travelling to Galba's palace...
+# ...Arriving to the Galba's palace.
+# Passing the guard at the entrance.
+# "Discipline!"
+# Looking for the message in the palace...
+# Ah, there it is! Seems cryptic.
+# [Game] Progress autosaved!
+# Deciphering Emperor's message...
+# Looks like I've got now the plain version copy of the Emperor's message.
+# Time to leave...
+# Travel ending.
+
+
 LOCATIONS =("home","Galba's palace","Otho's palace","Vitellius' palace","Vespasian's palace")
 LOWER_ALPHABETS = "abcdefghijklmnopqrstuvwxyz"
 UPPER_ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def progress():
+def read_progress():
     pass
 
 def shiftCharacter(Character, Alphabets, Shift: int = 13):
@@ -68,13 +107,13 @@ def rot13(Content):
             shifted_Content += shiftCharacter(i, LOWER_ALPHABETS)
     return shifted_Content
 
-def locate_message():
+def locate_message(next_location, passphrase):
     pass
 
-def append_progress():
+def append_progress(current_location, next_location, passphrase):
     pass
 
-def save_plain():
+def save_plain(next_location, plain_passphrase, plain_message):
     pass
 
 def main():
